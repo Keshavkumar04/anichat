03e5466d033a6337e296daa9a43d28a4f5179289
Keshavkumar04 <97456331+Keshavkumar04@users.noreply.github.com>
2023-08-01 23:35:36 +0530

    Update client.js

--- js/client.js
+++ js/client.js
@@ -1,5 +1,5 @@
 // const socket = io('http://localhost:3000');
-const socket = io('/');
+const socket = io('https://anichat.onrender.com');
 
 const form = document.querySelector('#formsubmit');
 const messageInput =  document.querySelector('#text');
