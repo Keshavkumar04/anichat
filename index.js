16ee355abcee89d5511d8925c0ec0b804f8b9e17
Keshavkumar04 <97456331+Keshavkumar04@users.noreply.github.com>
2023-08-01 22:58:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@
 //   }
 // });
 
-
-const io = require('socket.io')(3000, {
+const port = process.env.PORT || 3000;
+const io = require('socket.io')(port, {
   cors: {
     origin: '*',
     methods: ['GET', 'POST'],
